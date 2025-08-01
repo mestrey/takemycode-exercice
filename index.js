b0729e19862cc9f99199c6013750c21624ea67f1
mestrey <yovan@ecovolve.fr>
2025-08-01 18:34:15 +0300

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const PORT = 1234;
+const PORT = process.env.PORT || 1234;
 const MAX_ITEMS = 1_000_000;
 
 const app = express();
